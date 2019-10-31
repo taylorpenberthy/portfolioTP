3c80d2eddc234005e84e329cb21a0ff3b170e88a
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-10-31 15:04:33 -0400

    publish to github pages

---
+++ src/About.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import me from './moi.jpg';
+import me2 from './moi circle.png'
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+         <div class="container">
+            <div class="mover">
+             <img src={me2}alt="myface"/>
+       
+                
+            </div>
+                </div>
+        <div className='hero'>
+          <h1 className='projectstitle'>WHO AM I?</h1>
+        </div>
+        <div className='profilepic'>
+          <img src={me} className='propic' />
+          <p className='description'>
+            I'm Taylor. I'm a software engineer with a passion for music,
+            baking, coding, and exercise (among other things).
+          </p>
+        </div>
+        <div className='songbox'>
+          <h5>Peep my current favorite song!</h5>
+          <iframe
+            src='https://open.spotify.com/embed/track/5M4yti0QxgqJieUYaEXcpw'
+            width='300'
+            height='80'
+            frameborder='0'
+            allowtransparency='true'
+            allow='encrypted-media'
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/Floorplan.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"
+import "./App.css"
+import me from "./moi circle.png"
+class Floorplan extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            center: true
+        }
+    }
+    
+    render() { 
+       
+            return ( 
+                <div>
+                <h1>TP</h1>
+                <h3>(not the<img src="https://www.stickpng.com/assets/images/589395bc8370b70e212f3e63.png"></img>)</h3>
+                <div>
+               
+                </div>
+                </div>
+         
+             )
+        
+    }
+}
+
+export default Floorplan;
