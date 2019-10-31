5721ec662a9744689e3d7392d2a0af3ad387fd9f
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-10-31 16:41:32 -0400

    Updates

---
+++ precache-manifest.c89e81d26191132e1e1a6bca2f1fcd52.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af265f2444ae4cf7417101459be8bf50",
+    "url": "/portfolioTP.git/index.html"
+  },
+  {
+    "revision": "5a15bc6b47660a308105",
+    "url": "/portfolioTP.git/static/css/main.7f329128.chunk.css"
+  },
+  {
+    "revision": "c3d5be9bb1e71e5e9bde",
+    "url": "/portfolioTP.git/static/js/2.0c31c5ea.chunk.js"
+  },
+  {
+    "revision": "5a15bc6b47660a308105",
+    "url": "/portfolioTP.git/static/js/main.19d6474d.chunk.js"
+  },
+  {
+    "revision": "54c457ad1a00013fbb00",
+    "url": "/portfolioTP.git/static/js/runtime-main.6ae65df8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolioTP.git/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3d9fb15186f4f29af16a45bc26d1d493",
+    "url": "/portfolioTP.git/static/media/moi circle.3d9fb151.png"
+  },
+  {
+    "revision": "18d8165d04d07a53f79b3b29eacc9c8b",
+    "url": "/portfolioTP.git/static/media/moi.18d8165d.jpg"
+  },
+  {
+    "revision": "9c01ee82237496f360335601e400d06e",
+    "url": "/portfolioTP.git/static/media/sounddumpreal.9c01ee82.png"
+  },
+  {
+    "revision": "b3985a8689a6beee312af80f1ff3e118",
+    "url": "/portfolioTP.git/static/media/trivia1.b3985a86.png"
+  },
+  {
+    "revision": "93f1659dabc6fa820e9c17598ef9ca24",
+    "url": "/portfolioTP.git/static/media/yeetbeer.93f1659d.png"
+  }
+]);
