ce252c6e650916a496929632999510aa5aab0777
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-10-31 20:39:58 -0400

    Updates

---
+++ precache-manifest.354cde51e619148f9f279c2ad3cefbde.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b87a37c1b46b2e6aedb942bf88c0d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6ddded68f424ff13642",
+    "url": "/static/css/main.7f329128.chunk.css"
+  },
+  {
+    "revision": "c3d5be9bb1e71e5e9bde",
+    "url": "/static/js/2.0c31c5ea.chunk.js"
+  },
+  {
+    "revision": "f6ddded68f424ff13642",
+    "url": "/static/js/main.64593bdb.chunk.js"
+  },
+  {
+    "revision": "cdc8a44177ebaf5e659e",
+    "url": "/static/js/runtime-main.3d3cf7f1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3d9fb15186f4f29af16a45bc26d1d493",
+    "url": "/static/media/moi circle.3d9fb151.png"
+  },
+  {
+    "revision": "18d8165d04d07a53f79b3b29eacc9c8b",
+    "url": "/static/media/moi.18d8165d.jpg"
+  },
+  {
+    "revision": "9c01ee82237496f360335601e400d06e",
+    "url": "/static/media/sounddumpreal.9c01ee82.png"
+  },
+  {
+    "revision": "b3985a8689a6beee312af80f1ff3e118",
+    "url": "/static/media/trivia1.b3985a86.png"
+  },
+  {
+    "revision": "93f1659dabc6fa820e9c17598ef9ca24",
+    "url": "/static/media/yeetbeer.93f1659d.png"
+  }
+]);
