ef5c5b288d8cb232a40306d6cf88e24e670d030c
TAYLORPENBERTHY <taylor.penberthy@gmail.com>
2019-12-13 11:23:21 -0500

    changed contact button

--- src/About.js
+++ src/About.js
@@ -9,9 +9,9 @@ class About extends Component {
     return (
       <div>
         <div className='container'>
-          <div className='mover'>
+          {/* <div className='mover'>
             <img src={me2} alt='myface' className='mover' />
-          </div>
+          </div> */}
         </div>
         <div className='hero'>
           <h1 className='projectstitle'>WHO AM I?</h1>
@@ -19,19 +19,35 @@ class About extends Component {
         <div className='profilepic'>
           <img src={me} className='propic' />
           <p className='description'>
-            I'm Taylor. I'm a software engineer with a passion for music,
-            baking, coding, and exercise (among other things).
+            I am a software developer who has built projects that align with my interests in music,
+            baking, and exercise (among other things, I'm not THAT boring).
             <br />
             I've built multiple fullstack applications using the latest
             technologies, languages, and tools to develop meaningful projects.
             <br />
             The application and implementation of technology in science and
             healthcare fascinates me and encourages me to reimagine medicine
-            through a technological perspective.
+            through a technological perspective.  
           </p>
         </div>
-        <div className='songbox'>
-          <h5>Peep my current favorite song!</h5>
+        <div className='bottomofprojpage'>
+       
+          <div className='songbox'>
+          <iframe
+            src='https://open.spotify.com/embed/track/5M4yti0QxgqJieUYaEXcpw'
+            width='80'
+            height='80'
+            frameborder='0'
+            allowtransparency='true'
+            allow='encrypted-media'
+          ></iframe>
+
+        </div>
+        
+        <div className='contactbox'>
+         <a className='child' href='mailto:taylor.penberthy@gmail.com'><div className='child'>Contact Me!</div></a>
+        {/* <h5>Contact Me!</h5> */}
+{/*           
           <iframe
             src='https://open.spotify.com/embed/track/5M4yti0QxgqJieUYaEXcpw'
             width='300'
@@ -39,7 +55,8 @@ class About extends Component {
             frameborder='0'
             allowtransparency='true'
             allow='encrypted-media'
-          ></iframe>
+          ></iframe> */}
+        </div>
         </div>
       </div>
     );
